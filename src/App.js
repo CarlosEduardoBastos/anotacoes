56d0d0f65a2b5c13e227202cd0a7023cbb0f8ffa
Carlos Eduardo <dudu.lima.bastos@gmail.com>
2021-03-03 01:40:06 -0300

    adicionando os hooks e a boxAnotacoes e o estilo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+import React from 'react';
 import './App.css';
 
 function App() {
+  const [tarefas, setTarefas] = React.useState([]);
+  const [modal, setModal] = React.useState(false);
   return (
     <div className="App">
-
+      <div className="boxAnotacoes">
+        
+      </div>
     </div>
   );
 }
